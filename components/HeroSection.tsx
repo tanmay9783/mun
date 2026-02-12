33222e10f80ce2df23a91acd249793c6bf710a7a
tanmay9783 <tanmayagrawal0412@gmail.com>
2026-02-12 19:26:05 +0530

    Updated Hero sec

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -59,8 +59,9 @@ export default function HeroContent(): React.ReactElement {
                 {/* KIET + Logo side by side */}
                 <div className="flex flex-row items-center justify-center gap-2 sm:gap-4 md:gap-6 mb-10 sm:mb-[-8px]">
 
-                    {/* Logo - INCREASED SIZE */}
-                    <div className="relative w-[80px] h-[80px] sm:w-[90px] sm:h-[90px] md:w-[120px] md:h-[120px] lg:w-[130px] lg:h-[130px] flex-shrink-0">
+
+                    {/* Logo - Increased size */}
+                        <div className="relative w-[100px] h-[100px] sm:w-[110px] sm:h-[110px] md:w-[140px] md:h-[140px] lg:w-[150px] lg:h-[150px] flex-shrink-0">
                         <Image
                             src="/log.png"
                             alt="KIET MUN Logo"
